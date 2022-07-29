7ea44627956fb6d3d732e64146de4758bc04b01b
DmitriyR90 <ryabokon.dn@gmail.com>
2022-07-29 23:05:20 +0300

    hw made v1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+const greateGallery = greateGalaryMarkup(galleryItems)
+
+gallery.addEventListener('click', onImageClick)
+
+gallery.innerHTML = greateGallery
+
+function greateGalaryMarkup (items){
+    return items.map(item=> `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>` ).join('')
+}
+  
+function onImageClick(e){
+    e.preventDefault()
+
+    if (e.target.nodeName !== 'IMG'){
+             return
+    }
+    let imgOriginalUrl = e.target.dataset.source
+   
+    const instance = basicLightbox.create(`
+    <img src="${imgOriginalUrl}">`)
+    instance.show()
+   
+    gallery.addEventListener('keydown',  onKeyPress)
+
+    function onKeyPress(e){
+if(e.code==='Escape'){
+               instance.close();
+        gallery.removeEventListener('keydown',  onKeyPress)
+    }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery')
+
+const createGallery = createGalleryMarkup(galleryItems)
+
+galleryConteiner.innerHTML = createGallery
+
+function createGalleryMarkup(items){
+    return items.map((item) => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>` ).join('')
+}
+
+let gallery = new SimpleLightbox('.gallery a', { 
+    captionType: 'attr', 
+    captionsData : 'alt',
+    captionDelay: 250,
+     
+});
